8dd042e319f4cc5eaa4e46d1f1ac7965ccdbe22f
Edgar_olg <a01567109@tec.mx>
2025-01-02 19:27:28 -0600

    inicio del front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,65 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='headerContainer'>
+          <h2 className='header-title'>ToDo-List</h2>
+          <div className='headerLinks'>
+            <p>aqui van mis links de correo, linkedin, git hub</p>
+          </div>
+        </div>
       </header>
+      <div className='generalContainer'>
+        <h1 className='generalTitle'>Task</h1>
+        <div className='componentsContainer'>
+          <div className='filtersContainer'>
+            <div className='statusContainer'>
+              <p className='titleFilters'>
+                Status
+              </p>
+              <span className='titleLineDecorative'></span>
+              <div className='optionsContainer'>
+                <button className='statusOption statusComplete'>
+                  Complete
+                </button>
+                <button className='statusOption statusProgress'>
+                  In Progress
+                </button>
+                <button className='statusOption statusPending'>
+                  Pending
+                </button>
+              </div>
+            </div>
+            <div className='categoriesContainer'>
+              <p className='titleFilters'>
+                Categories
+              </p>
+              <span className='titleLineDecorative'></span>
+              <div className='optionsContainerCategories'>
+                <button className='categoriesOption'>
+                  Work
+                </button>
+                <button className='categoriesOption'>
+                  School
+                </button>
+                <button className='categoriesOption'>
+                  GYM
+                </button>
+                <button className='categoriesOption'>
+                  Life
+                </button>
+                <button className='categoriesOption'>
+                  Investments
+                </button>
+                <button className='categoriesOption'>
+                  Hobby
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='tableContainer'>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
