d4f180d362ec265a62a524aace456557c7c9cd0c
Edgar Olg <edgar.fer.a01@gmail.com>
2025-01-17 14:21:36 -0600

    configuracion git

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,36 @@
-
 import './App.css';
 import CategoriesFilter from './components/Filters/CategoriesFilter';
 import TaskList from './components/TaskList';
 import StatusFilter from './components/Filters/StatusFilter';
+import { useState } from 'react';
 
 function App() {
+  // Estados para el filtro
+  const [selectedStatus, setSelectedStatus] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  // Funciones para actualizar el filtro de estado y categoría
+  const handleStatusFilterChange = (status) => {
+    setSelectedStatus(status); // Actualiza el filtro de estado
+  };
+
+  const handleCategoryFilterChange = (category) => {
+    setSelectedCategory(category); // Actualiza el filtro de categoría
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div className='headerContainer'>
           <h2 className='header-title'>ToDo-List</h2>
           <div className='headerLinks'>
-            <p>aqui van mis links de correo, linkedin, git hub</p>
+            <ul>
+              <li>
+                correo
+              </li>
+              <li>linkedin</li>
+              <li>git hub</li>
+            </ul>
           </div>
         </div>
       </header>
@@ -19,11 +38,11 @@ function App() {
         <h1 className='generalTitle'>Task</h1>
         <div className='componentsContainer'>
           <div className='filtersContainer'>
-            <StatusFilter />
-            <CategoriesFilter />
+            <StatusFilter filterTasks={handleStatusFilterChange} />
+            <CategoriesFilter filterCategories={handleCategoryFilterChange} />
           </div>
           <div className='tableContainer'>
-            <TaskList />
+            <TaskList status={selectedStatus} category={selectedCategory} />
           </div>
         </div>
       </div>
